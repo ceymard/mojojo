8281487a5accfff139c6fbc1891a86e6fa98c25e
Christophe Eymard <christophe.eymard@sales-way.com>
2017-03-27 18:20:51 +0200

    yml not necessary anymore

--- themes/pouet.js
+++ themes/pouet.js
@@ -1,10 +1,10 @@
+'use strict'
 
 const fs = require('fs')
 const y = require('js-yaml')
+const c = require('color')
 
-var base = y.load(fs.readFileSync('mojo.yml', 'utf-8'))
-
-// console.log(base)
+// var base = y.load(fs.readFileSync('mojo.yml', 'utf-8'))
 
 function unescape(val) {
   return (val||'').toString().replace(/_([\w\d]{3,8})/g, m => '#' + m.slice(1))
@@ -12,7 +12,7 @@ function unescape(val) {
 
 function dct(val, _) {
   _ = _ || ''
-  if (typeof val === 'string' || !val || typeof val !== 'object')
+  if (typeof val === 'string' || !val || typeof val !== 'object' || val.constructor !== Object)
     return `${_}<string>${unescape(val)}</string>\n`
 
   var res = `${_}<dict>\n`
@@ -24,28 +24,158 @@ function dct(val, _) {
   return res
 }
 
-var scopes = []
-for (var scope in base.scopes) {
-  var vals = scope.split(' ')
-  var settings = {}
-  var spec = {settings: settings}
+class S {
 
-  while (['italic', 'underline', 'bold'].indexOf(vals[0]) !== -1) {
-    settings.fontStyle = vals[0]
-    vals = vals.slice(1)
+  constructor() {
+    this.settings = {}
+    this.scopes = []
   }
 
-  settings.foreground = unescape(vals[0])
-  if (vals[1])
-    settings.background = unescape(vals[1])
-  else
-    settings.background = unescape(vals[0] + 'a0')
+  static from(color) {
+    var res = new S()
+    res.settings.foreground = color.hex()
+    S.scopes.push(res)
+    return res
+  }
 
-  spec.scope = base.scopes[scope].join(', ')
-  spec.name = '...'
+  bg(col) {
+    this.settings.background = col.hex()
+    return this
+  }
 
-  scopes.push(spec)
+  italic() {
+    this.settings.fontStyle = 'italic'
+    return this
+  }
+
+  bold() {
+    this.settings.fontStyle = 'bold'
+    return this
+  }
+
+  add() {
+    this.scopes = this.scopes.concat(Array.prototype.slice.call(arguments))
+  }
+
+  render(indent) {
+    return dct({
+      settings: this.settings,
+      name: '...',
+      scope: this.scopes.join(', ')
+    }, indent)
+  }
 }
+
+S.scopes = []
+
+/////////////////////////////////////////////////
+
+var NAME = 'My Mojo'
+
+var FG = c('#eeeeee')
+
+// Base color
+
+// blueish
+var FN = c('#68c4ff').saturate(0.2).lighten(0.15)
+
+var TSX = FN.rotate(180)
+var TYPES = FN.rotate(-60)
+var BASE = FN.rotate(20)
+var CONSTANT = FN.rotate(230).desaturate(0.4)
+
+var COMMENT = FN.darken(0.5).desaturate(0.7)
+var BG = FN.darken(0.9).desaturate(0.9)
+
+var settings = {
+  foreground: FG.hex(),
+  background: BG.hex(),
+  caret: FG.hex(),
+  invisibles: FG.hex(),
+  bracketContentsOptions: 'underline',
+  tagsOptions: 'underline'
+  // lineHighlight:
+  // selection:
+  // selectionBorder:
+  // tagsForeground:
+}
+
+S.from(COMMENT).italic().add(
+  'comment',
+  'punctuation.definition.comment'
+)
+
+S.from(BASE).add(
+  'keyword.control',
+  'storage.type',
+  'storage.modifier'
+)
+
+S.from(FG).add(
+  'string variable',
+  'string punctuation.accessor',
+  'string keyword'
+)
+
+S.from(FN.lighten(0.15)).add(
+  'meta.definition.variable',
+  'meta.parameters punctuation',
+  'meta.parameters punctuation.definition',
+  'variable.parameter'
+)
+
+S.from(BASE.lighten(0.15)).add(
+  'meta.objectliteral punctuation.definition',
+  'meta.objectliteral meta.object-literal.key'
+)
+
+S.from(TYPES).add(
+  'meta.type.annotation',
+  'entity.name.type',
+  'meta.return.type',
+  'meta.type.parameters'
+)
+
+S.from(TYPES.lighten(0.2)).add(
+  'meta.type.annotation keyword',
+  'meta.type.annotation meta.brace',
+  'meta.type.annotation punctuation',
+  'meta.type.parameters punctuation',
+  'meta.type.parameters meta.brace'
+)
+
+S.from(TYPES.darken(0.2)).add(
+  'entity.name.type.class'
+)
+
+S.from(FN).add(
+  'meta.definition entity.name.function',
+  'entity.name.function',
+  'support.function'
+)
+
+
+S.from(TSX).add(
+  'entity.name.tag'
+)
+
+S.from(TSX.lighten(0.1)).add(
+  'entity.other.attribute-name',
+  'meta.tag punctuation.section',
+  'meta.tag punctuation.definition',
+  'meta.tag keyword.operator.assignment'
+)
+
+S.from(CONSTANT).add(
+  'meta.embedded string',
+  'string',
+  'constant',
+  'punctuation.definition.string'
+)
+
+S.from(CONSTANT.darken(0.3).saturate(0.3)).add(
+  'string punctuation.definition.template-expression'
+)
 
 var res = `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist
@@ -54,17 +184,16 @@ var res = `<?xml version="1.0" encoding="UTF-8"?>
 <plist version="1.0">
   <dict>
     <key>name</key>
-    <string>${base.name}</string>
+    <string>${NAME}</string>
     <key>comment</key>
-    <string>${base.comment || ''}</string>
+    <string></string>
     <key>settings</key>
     <array>
-${dct({settings: base.settings}, '      ')}
-${scopes.map(s => dct(s, '      ')).join('\n')}
+${dct({settings: settings}, '      ')}
+${S.scopes.map(s => s.render('       ')).join('\n')}
     </array>
   </dict>
 </plist>
 `
 
-console.log(res)
 fs.writeFileSync('mojo.tmTheme', res, {encoding: 'utf-8'})
