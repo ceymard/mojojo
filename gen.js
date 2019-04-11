df26ea04ce657498d6cfcd299a5dc8b2a54c906d
Christophe Eymard <christophe.eymard@sales-way.com>
2019-04-11 11:27:38 +0200

    new way of generating colors

--- gen.js
+++ gen.js
@@ -1,9 +1,10 @@
 'use strict'
 
+const namer = require('color-namer')
 const fs = require('fs')
-const y = require('js-yaml')
 const c = require('chroma-js')
 const flat = require('flat')
+const path = require('path')
 
 // var base = y.load(fs.readFileSync('mojo.yml', 'utf-8'))
 
@@ -139,258 +140,286 @@ class Color {
 
 /////////////////////////////////////////////////
 
-var NAME = 'My Mojo'
 
-var FG = new C(c('#eeeeee'))
-var _ = c.hcl(240, 90, 60)
+function generate() {
+  const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json')))
 
-// Base color
-
-// blueish
-// var BASE = c('#68c4ff').saturate(0.2).lighten(0.15)
-var BASE_LUM = 60
-var BASE = new C(c(270, 80, 80, 'hcl')).rotate(parseInt(process.argv[2] || 0))
-
-
-// var FN = c('#80defa')
-// var FN = c('#80cbc4')
-
-var OPPOSITE = BASE.rotate(-180)
-var TYPES = BASE.rotate(-90).addchr(30)
-var STRING = BASE.rotate(90)
-var CONSTANT = BASE.rotate(135)
-
-var COMMENT = BASE.lum(40).desaturate(2)
-var BG = BASE.lum(5).desaturate(3)
-
-
-/////////////////////////////////////////////////////////////////////
-
-const BORDER = BG.addlum(-30)
-
-var colors = {
-  foreground: FG,
-  'widget.shadow': BG.lum(5),
-  activityBar: {
-    background: BG,
-    foreground: FG.lum(BASE_LUM),
-    border: BORDER
-  },
-  activityBarBadge: {
-    background: BASE.lum(50),
-    foreground: BASE.lum(40)
-  },
-  sideBar: {
-    background: BG,
-    foreground: FG,
-    border: BORDER
-  },
-  sideBarSectionHeader: {
-    background: BASE.lum(5),
-    foreground: FG
-  },
-  editorGroupHeader: {
-    tabsBackground: BG
-  },
-  tab: {
-    inactiveBackground: BG,
-    inactiveForeground: FG.lum(40),
-    activeBackground: BG,
-    activeForeground: FG,
-    border: BORDER
-  },
-  editor: {
-    foreground: FG,
-    background: BG,
-    lineHighlightBackground: BASE.lum(1),
-    selection: BASE.lum(15),
-    selectionBorder: BASE.lum(23),
-  },
-  editorBracketMatch: {
-    background: BASE.lum(20),
-    border: BASE.lum(25)
-  },
-  statusBar: {
-    background: BASE.lum(40)
-  },
-
-  // VSCode
-  'editorIndentGuide.background': BG.lum(5)
-
-  // tagsForeground:
-}
-
-
-/////////////////////////////////////////////////////////////////////
-
-// Comments
-S.from(COMMENT).italic().add(
-  'comment',
-  'punctuation.definition.comment',
-  'string.quoted.docstring',
-  'string.quoted.docstring punctuation.definition.string'
-)
-
-// Keywords
-S.from(BASE).add(
-  'keyword',
-  'storage.modifier',
-  'storage.type'
-)
-
-S.from(FG).add(
-  'string variable',
-  'string punctuation.accessor',
-  'string keyword'
-)
-
-
-S.from(BASE.addlum(-5).addchr(-5)).add(
-  'meta.objectliteral punctuation.definition',
-  'meta.objectliteral meta.object-literal.key',
-  'meta.class variable.object.property'
-)
-
-// markdown
-S.from(BASE.addlum(10)).bold().add(
-  'markup.bold'
-)
-
-// markdown
-S.from(BASE.addlum(10)).italic().add(
-  'markup.italic'
-)
-
-S.from(TYPES).add(
-  'meta.type.annotation',
-  'entity.name.type',
-  'meta.return.type',
-  'meta.type.parameters',
-  'entity.other.inherited-class',
-  'support.type',
-  'storage.type.postgres',
-
-  // markdown
-  'markup.list beginning.punctuation'
-)
-
-S.from(TYPES.addlum(-5).addchr(-5)).add(
-  'meta.indexer.declaration variable.parameter',
-  'meta.object.type punctuation.definition',
-  'meta.type.annotation variable.object.property',
-  'punctuation.definition.typeparameters',
-  'meta.type.annotation keyword',
-  'meta.type.annotation meta.brace',
-  'meta.type.annotation punctuation',
-  'meta.type.parameters punctuation',
-  'meta.type.parameters meta.brace',
-
-  // markdown
-  'markup.quote'
-)
-
-S.from(TYPES).add(
-  'entity.name.type.class'
-)
-
-S.from(BASE).add(
-  // 'support.function',
-  // markdown
-  'markup.heading'
-)
-
-S.from(BASE.addlum(-5).addchr(-5)).add(
-  'meta.definition.variable',
-  'meta.parameters punctuation',
-  'meta.parameters punctuation.definition',
-  'variable.parameter',
-  // Markdown
-  'markup.heading punctuation'
-)
-
-S.from(OPPOSITE).add(
-  'entity.name.tag',
-  'markup.inline punctuation',
-  'markup.fenced_code punctuation'
-)
-
-S.from(BASE.rotate(-45).addchr(20)).add(
-  'meta.definition entity.name.function',
-  'entity.name.function',
-  'support.function',
-  'meta.function-call.tsx support.function.dom.tsx'
-)
-
-
-S.from(OPPOSITE.addlum(-5).addchr(-5)).add(
-  'entity.other.attribute-name',
-  'meta.tag punctuation.section',
-  'meta.tag punctuation.definition',
-  'meta.tag keyword.operator.assignment',
-  // markdown
-  'markup.inline',
-  'markup.fenced_code'
-)
-
-S.from(STRING).add(
-  'meta.embedded string',
-  'punctuation.definition.string',
-  'string'
-)
-
-S.from(STRING.addlum(5)).add(
-  'meta.link.inline.markdown',
-  'meta.link.inline.markdown punctuation.definition.string',
-  'meta.image.inline',
-  'meta.image.inline punctuation.definition.string'
-)
-
-S.from(STRING.addlum(-5).addchr(-5)).add(
-  'string punctuation.definition.template-expression'
-)
-
-S.from(CONSTANT).add(
-  'constant',
-  'variable.language'
-)
-
-
-// var res = `<?xml version="1.0" encoding="UTF-8"?>
-// <!DOCTYPE plist
-//   PUBLIC '-//Apple//DTD PLIST 1.0//EN'
-//   'http://www.apple.com/DTDs/PropertyList-1.0.dtd'>
-// <plist version="1.0">
-//   <dict>
-//     <key>name</key>
-//     <string>${NAME}</string>
-//     <key>comment</key>
-//     <string></string>
-//     <key>settings</key>
-//     <array>
-// ${dct({settings: settings}, '      ')}
-// ${S.scopes.map(s => s.render('       ')).join('\n')}
-//     </array>
-//   </dict>
-// </plist>
-// `
-
-function hexify(dct) {
-  for (var x in dct) {
-    if (typeof dct[x].hex === 'function') {
-      dct[x] = dct[x].hex()
-    } else if (dct[x].constructor === Object) {
-      hexify(dct[x])
-    }
+  const themes = []
+  for (var i = 0; i < 360; i += 30) {
+    // themes.push(c(60, 40, i, 'lch'))
+    themes.push(c(80, 60, i, 'lch'))
   }
-  return dct
+
+  // const themes = [
+  //   c(60, 40, 0, 'lch'),
+  //   c(60, 40, 60, 'lch'),
+  //   c(60, 40, 120, 'lch'),
+  //   c(60, 40, 180, 'lch'),
+  //   c(60, 40, 240, 'lch'),
+  //   c(60, 40, 300, 'lch'),
+  // ]
+
+  for (var t of themes) {
+    const theme = make_theme(t)
+    fs.writeFileSync(path.join(__dirname, `./themes/${t.hex().replace('#', '')}.json`), theme, 'utf-8')
+  }
+
+  pkg.contributes.themes = themes.map(t => { return {
+    label: `Mojojo ${namer(t.hex()).pantone[0].name.replace(/^(.)/, m => m.toUpperCase())}`,
+    uiTheme: 'vs-dark',
+    path: `./themes/${t.hex().replace('#', '')}.json`
+  } })
+
+  fs.writeFileSync(path.join(__dirname, './package.json'), JSON.stringify(pkg, null, 2), 'utf-8')
+
 }
 
-var res = {
-  type: 'dark',
-  colors: flat.flatten(hexify(colors)),
-  tokenColors: S.scopes.map(s => s.renderObj())
-}
+generate()
 
-// console.log(res)
-console.log(JSON.stringify(res, null, 2))
-// fs.writeFileSync('mojo.tmTheme', res, {encoding: 'utf-8'})
+function make_theme(color) {
+
+  var BASE = new C(c(color))
+
+  const BASE_DARKER = BASE.lum(25).chr(10)
+  const BORDER = BASE.lum(20).chr(5)
+
+  var FG = new C(c('#ffffff'))
+
+  var BG = BASE.lum(5).chr(1)
+
+  var OPPOSITE = BASE.rotate(-180)
+  var TYPES = BASE.rotate(-80)
+  var FUNCTIONS = BASE.rotate(30).chr(40)
+  var STRING = BASE.rotate(60)
+  var CONSTANT = BASE.rotate(120)
+
+  var COMMENT = BASE.lum(40).chr(10)
+
+  /////////////////////////////////////////////////////////////////////
+
+  var colors = {
+    foreground: FG,
+    'widget.shadow': BG.lum(5),
+    activityBar: {
+      background: BASE_DARKER,
+      foreground: FG,
+      border: BORDER
+    },
+    activityBarBadge: {
+      background: OPPOSITE,
+      foreground: FG
+    },
+    sideBar: {
+      background: BG,
+      foreground: FG,
+      border: BORDER
+    },
+    sideBarSectionHeader: {
+      background: BASE_DARKER,
+      foreground: FG
+    },
+    editorGroupHeader: {
+      tabsBackground: BG
+    },
+    tab: {
+      inactiveBackground: BG,
+      inactiveForeground: FG.lum(40),
+      activeBackground: BG,
+      activeForeground: FG,
+      border: BORDER
+    },
+    editor: {
+      foreground: FG,
+      background: BG,
+      lineHighlightBackground: BASE.lum(1),
+      selection: BASE.lum(15),
+      selectionBorder: BASE.lum(23),
+    },
+    editorBracketMatch: {
+      background: BASE.lum(20),
+      border: BASE.lum(25)
+    },
+    statusBar: {
+      background: BASE_DARKER,
+      border: BORDER
+    },
+
+    // VSCode
+    'editorIndentGuide.background': BG.lum(5)
+
+    // tagsForeground:
+  }
+
+
+  /////////////////////////////////////////////////////////////////////
+
+  // Comments
+  S.from(COMMENT).italic().add(
+    'comment',
+    'punctuation.definition.comment',
+    'string.quoted.docstring',
+    'string.quoted.docstring punctuation.definition.string'
+  )
+
+  // Keywords
+  S.from(BASE).add(
+    'keyword',
+    'storage.modifier',
+    'storage.type'
+  )
+
+  S.from(FG).add(
+    'string variable',
+    'string punctuation.accessor',
+    'string keyword'
+  )
+
+
+  S.from(BASE.addlum(10)).add(
+    'meta.objectliteral punctuation.definition',
+    'meta.objectliteral meta.object-literal.key',
+    'meta.class variable.object.property'
+  )
+
+  // markdown
+  S.from(BASE.addlum(10)).bold().add(
+    'markup.bold'
+  )
+
+  // markdown
+  S.from(BASE.addlum(10)).italic().add(
+    'markup.italic'
+  )
+
+  S.from(TYPES).add(
+    'meta.type.annotation',
+    'entity.name.type',
+    'meta.return.type',
+    'meta.type.parameters',
+    'entity.other.inherited-class',
+    'support.type',
+    'storage.type.postgres',
+
+    // markdown
+    'markup.list beginning.punctuation'
+  )
+
+  S.from(TYPES.addlum(10)).add(
+    'meta.indexer.declaration variable.parameter',
+    'meta.object.type punctuation.definition',
+    'meta.type.annotation variable.object.property',
+    'punctuation.definition.typeparameters',
+    'meta.type.annotation keyword',
+    'meta.type.annotation meta.brace',
+    'meta.type.annotation punctuation',
+    'meta.type.parameters punctuation',
+    'meta.type.parameters meta.brace',
+
+    // markdown
+    'markup.quote'
+  )
+
+  const SQL = TYPES.rotate(-85)
+  S.from(SQL).add(
+    'source.ts source.pgsql keyword',
+    'source.ts keyword.other.DML.sql',
+    'source.ts keyword.other.create.sql',
+    'source.ts keyword.other.order.sql',
+  )
+
+  S.from(SQL.addchr(-30)).add(
+    'source.ts keyword.other.DDL.create.II.sql',
+    'source.ts keyword.other.sql',
+    'source.ts keyword.other.alias.sql',
+    'source.ts support.function.expression.sql',
+    'source.ts constant.other.database-name.sql',
+  )
+
+  S.from(SQL.addlum(15)).add(
+    'source.ts constant.other.table-name.sql',
+  )
+
+  S.from(TYPES.addlum(-5)).add(
+    'entity.name.type.class'
+  )
+
+  S.from(BASE).add(
+    'support.function',
+    // markdown
+    'markup.heading'
+  )
+
+  S.from(BASE.addlum(5)).add(
+    'meta.definition.variable',
+    'meta.parameters punctuation',
+    'meta.parameters punctuation.definition',
+    'variable.parameter',
+    // Markdown
+    'markup.heading punctuation'
+  )
+
+  S.from(OPPOSITE).add(
+    'entity.name.tag',
+    'markup.inline punctuation',
+    'markup.fenced_code punctuation'
+  )
+
+  S.from(FUNCTIONS).add(
+    'meta.definition entity.name.function',
+    'entity.name.function',
+    'support.function'
+  )
+
+
+  S.from(OPPOSITE.addlum(5)).add(
+    'entity.other.attribute-name',
+    'meta.tag punctuation.section',
+    'meta.tag punctuation.definition',
+    'meta.tag keyword.operator.assignment',
+    // markdown
+    'markup.inline',
+    'markup.fenced_code'
+  )
+
+  S.from(STRING).add(
+    'meta.embedded string',
+    'punctuation.definition.string',
+    'string'
+  )
+
+  S.from(STRING.addlum(5)).add(
+    'meta.link.inline.markdown',
+    'meta.link.inline.markdown punctuation.definition.string',
+    'meta.image.inline',
+    'meta.image.inline punctuation.definition.string'
+  )
+
+  S.from(STRING.addlum(-5)).add(
+    'string punctuation.definition.template-expression'
+  )
+
+  S.from(CONSTANT.addlum(5)).add(
+    'constant',
+    'variable.language'
+  )
+
+
+  function hexify(dct) {
+    for (var x in dct) {
+      if (typeof dct[x].hex === 'function') {
+        dct[x] = dct[x].hex()
+      } else if (dct[x].constructor === Object) {
+        hexify(dct[x])
+      }
+    }
+    return dct
+  }
+
+  var res = {
+    type: 'dark',
+    colors: flat.flatten(hexify(colors)),
+    tokenColors: S.scopes.map(s => s.renderObj())
+  }
+
+  return JSON.stringify(res, null, 2)
+}
